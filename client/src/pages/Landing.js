24678ec0cd637b0aa8087ee50ca289682280ea1a
mxdi9i7 <peter.zheng88228@gmail.com>
2019-06-09 19:51:50 -0400

    final update

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -11,7 +11,7 @@ class LandingPage extends Component {
     loginQR: "",
     rooms: [],
     msg: "",
-    timeout: "",
+    timeout: ""
   };
   componentDidMount() {
     this.pollForLoginState();
@@ -27,14 +27,13 @@ class LandingPage extends Component {
     setTimeout(() => {
       axios.get(apiUrl + "/rooms").then(resp => {
         if (resp.data.isRoomReady) {
-          console.log('is ready')
+          console.log("is ready");
           this.setState({ rooms: resp.data.data, isRoomReady: true });
         } else {
-          this.handleGetRoomsList()
+          this.handleGetRoomsList();
         }
-      });  
+      });
     }, 3000);
-    
   };
   handleInputChange = e => {
     this.setState({ msg: e.target.value });
@@ -43,21 +42,24 @@ class LandingPage extends Component {
     this.setState({ timeout: e.target.value });
   };
   handleSendMassMessage = () => {
-    this.setState({
-      isMessageInProcess: true
-    }, () => {
-      axios
-        .get(apiUrl + "/rooms/send", {
-          params: { msg: this.state.msg, timeout: this.state.timeout || 1 }
-        })
-        .then(resp => {
-          if (resp.data.data) {
-            this.setState({ msg: "", isMessageInProcess: false }, () => {
-              alert('全部信息发送完成')
-            });
-          }
-        });
-    })
+    this.setState(
+      {
+        isMessageInProcess: true
+      },
+      () => {
+        axios
+          .get(apiUrl + "/rooms/send", {
+            params: { msg: this.state.msg, timeout: this.state.timeout || 1 }
+          })
+          .then(resp => {
+            if (resp.data.data) {
+              this.setState({ msg: "", isMessageInProcess: false }, () => {
+                alert("全部信息发送完成");
+              });
+            }
+          });
+      }
+    );
   };
   pollForLoginState = () => {
     setTimeout(() => {
@@ -74,7 +76,15 @@ class LandingPage extends Component {
     }, 1000);
   };
   render() {
-    const { loginQR, isLoggedIn, isMessageInProcess, isRoomReady, rooms, msg, timeout } = this.state;
+    const {
+      loginQR,
+      isLoggedIn,
+      isMessageInProcess,
+      isRoomReady,
+      rooms,
+      msg,
+      timeout
+    } = this.state;
     return (
       <div>
         {!isLoggedIn && (
@@ -87,40 +97,36 @@ class LandingPage extends Component {
             </div>
           </div>
         )}
-        {
-          isLoggedIn && !isRoomReady && '正在加载中...'
-        }
-        {
-          isRoomReady && <div className="rooms-container">
-          <h2>所有可獲取群聊</h2>
-          {rooms.map((room, i) => (
-            <p key={i}>
-              {i + 1}. {room}
-            </p>
-          ))}
-        </div>
-        }
-        {
-          isMessageInProcess ? 
-          '消息正在发送中，请勿刷新页面' :
-          (
-            <div className="msg-form-container">
-              <input
-                type="number"
-                placeholder="間隔/秒 (默認1秒)"
-                onChange={this.handleTimeoutChange}
-                value={timeout}
-              />
-              <input
-                placeholder="群發消息文本"
-                type="text"
-                onChange={this.handleInputChange}
-                value={msg}
-              />
-              <button onClick={this.handleSendMassMessage}>send msg</button>
-            </div>
-          )
-        }``
+        {isLoggedIn && !isRoomReady && "正在加载中..."}
+        {isRoomReady && (
+          <div className="rooms-container">
+            <h2>所有可獲取群聊</h2>
+            {rooms.map((room, i) => (
+              <p key={i}>
+                {i + 1}. {room}
+              </p>
+            ))}
+          </div>
+        )}
+        {isMessageInProcess ? (
+          "消息正在发送中，请勿刷新页面"
+        ) : (
+          <div className="msg-form-container">
+            <input
+              type="number"
+              placeholder="間隔/秒 (默認1秒)"
+              onChange={this.handleTimeoutChange}
+              value={timeout}
+            />
+            <input
+              placeholder="群發消息文本"
+              type="text"
+              onChange={this.handleInputChange}
+              value={msg}
+            />
+            <button onClick={this.handleSendMassMessage}>send msg</button>
+          </div>
+        )}
       </div>
     );
   }
